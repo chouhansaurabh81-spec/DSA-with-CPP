d7a509984d126f97d6583f8008cb0145e8579a02
Saurabh Chouhan <chouhansaurabh81@gmail.com>
2026-07-17 22:20:39 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (19.22%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -8,8 +8,8 @@ public:
         int i=0;
         for(int j=0 ; j<n ; j++){
             if(nums[i] != nums[j]){
-                i++;
-                nums[i] = nums[j];
+                //i++;
+                nums[++i] = nums[j];
             }
         }
         return i+1;
